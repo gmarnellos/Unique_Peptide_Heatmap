6ae618bb302ba02cd2e38e0c15de8551ffe5ceae
gmarnellos <gmarnellos@gmail.com>
2015-05-26 11:42:21 -0400

    Changed output to show information (sequence and area) for each unique, non-redundant peptide, instead of just the number of unique peptides

--- UniquePeptideHeatmap.php
+++ UniquePeptideHeatmap.php
@@ -13,7 +13,7 @@
         
         // Needs more logging info and error trapping.  Works but is a bit flaky.  Some tests might be nice too.
         
-        var $protein_url = "http://www.uniprot.org/uniprot/{{acc}}";
+        //var $protein_url = "http://www.uniprot.org/uniprot/{{acc}}";
         var $format      = "csv ";
         var $files       = array();
         var $filenames   = array();
@@ -29,7 +29,7 @@
                 $this->filenames[] = basename($file);
             }
             
-            $this->outfile = "/tmp/combined_heatmap.". date("Y-m-d_H:m:i") . ".xls";
+            $this->outfile = "/tmp/combined_heatmap.unique_peptide_info.". date("Y-m-d_H:m:i") . ".xls";
             
             $this->str = "";
             
@@ -38,20 +38,14 @@
     
         public function parseFiles() {
         
-          // First we want to combine the datasets.   We want
-        
-          // Array of accession numbers (column A)
-          // Array of descriptions hashed by accession numbers
-          // Each accession is associated with one number from each of the files - unique peptides (Column F)
-        
-        
-          // Loop through rows
-          //   If we have some data in column A then :
-          //        - test for max and min peptides
+          // From the input data we need to get:
+          
+          // Peptide sequence and area information, for peptides unique to a protein group
+          // Protein group accession numbers
+          // Descriptions corresponding to accessions
+          
         
           $this->accessions = array();
-          $this->minpep     = 100000;
-          $this->maxpep     = -1;
             
           $this->minarea   = 1E20;
           $this->maxarea   = -1;
@@ -136,15 +130,18 @@
                       $num_unique_peptides = count($acc_contents['seqs']);
                       
                       $comp_area = 0;
+
+                      // go through unique, non-redundant peptides
                       foreach ($acc_contents['seqs'] as $pep => $pep_contents) {
-                          $comp_area += array_sum($pep_contents['area'])/count($pep_contents['area']);
+                          $new_description = "$acc : $description";
+                          $peptide_string = $pep;
+                          $comp_area = array_sum($pep_contents['area'])/count($pep_contents['area']);
+                          // process information of each peptide
+                          $this->process_accession($pep,$new_description,$comp_area);
                       }
-                      
+
                       //echo "Acc: $acc  Desc: $description  Num: $num_unique_peptides Area: $comp_area \n";
                     
-                      // ready to process accession information
-                      $this->process_accession($acc,$description,$num_unique_peptides,$comp_area);
-                      
                   }
                   
                   unset($data);    // This deletes the whole $data array
@@ -160,31 +157,29 @@
             
             $row = 2;
             
-            $minpep = $this->minpep;
-            $maxpep = $this->maxpep;
-            
             $minarea = $this->minarea;
             $maxarea = $this->maxarea;
             
-            $this->str .= "Min max $minpep  $maxpep\n";
+            //$this->str .= "Min max $minarea  $maxarea\n";
             
             $outobj = new PHPExcel();
             $outobj->setActiveSheetIndex(0);
-            $outobj->getActiveSheet()->setCellValue('A1','Accession');
-            $outobj->getActiveSheet()->setCellValue('B1','Description');
+            
+            $outobj->getActiveSheet()->setCellValue('A1','Peptide sequence');
+            $outobj->getActiveSheet()->setCellValue('B1','Accession : Description');
             
             $cols = array('C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ');
             
             foreach ($this->files as $i => $f) {
                 // Set column headings and widths
-                $outobj->getActiveSheet()->setCellValue($cols[$i] . "1",$this->filenames[$i]);
-                $outobj->getActiveSheet()->setCellValue($cols[$i+sizeof($this->files)]."1",$this->filenames[$i] . " Area");
-                $outobj->getActiveSheet()->getColumnDimension($cols[$i])->setWidth(20);
-                $outobj->getActiveSheet()->getColumnDimension($cols[$i+sizeof($this->files)])->setWidth(20);
+                $outobj->getActiveSheet()->setCellValue($cols[$i]."1",$this->filenames[$i] . " Area");
+                $outobj->getActiveSheet()->getColumnDimension($cols[$i])->setWidth(15);
+                
             }
             
-            $outobj->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
-            $outobj->getActiveSheet()->getColumnDimension('B')->setWidth(100);;
+            //$outobj->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+            $outobj->getActiveSheet()->getColumnDimension('A')->setWidth(45);
+            $outobj->getActiveSheet()->getColumnDimension('B')->setWidth(70);;
             
             $row = 2;
             
@@ -193,36 +188,17 @@
             foreach ($this->accessions as $acc => $vals) {
                 $desc = $vals['Description'];
                 
-                $unique_peptides = "-";
-                
-                $url = preg_replace("/{{acc}}/",$acc,$this->protein_url);
-                
+                //$unique_peptides = "-";
                 
                 $outobj->getActiveSheet()->setCellValue('A'.$row,$acc);
                 $outobj->getActiveSheet()->setCellValue('B'.$row,$desc);
                 
-                $outobj->getActiveSheet()->getCell('A'.$row)->getHyperlink()->setUrl("http://www.uniprot.org/uniprot/$acc");
+                //$url = preg_replace("/{{acc}}/",$acc,$this->protein_url);
+                //$outobj->getActiveSheet()->getCell('A'.$row)->getHyperlink()->setUrl("http://www.uniprot.org/uniprot/$acc");
                 
                 foreach ($this->files as $i=>$f) {
-                    
-                    if (isset($vals['Unique_Peptides'][$f])) {
-                        $unique_peptides = $vals['Unique_Peptides'][$f];
+                    if (isset($vals['Area'][$f])) {
                         
-                        // Calculate color for unique peptide count
-                        
-                        list($r,$g,$b) = $this->rgb($minpep,$maxpep,$unique_peptides);
-                        
-                        $col = $r . $g. $b;
-                        
-                        //print "Val $val - color $row $col\n";
-                        
-                        $this->cellColor($cols[$i].$row,$col,$outobj);
-                        $outobj->getActiveSheet()->setCellValue($cols[$i].$row,$unique_peptides);
-
-                        if (isset($vals['Area'][$f])) {
-                            if ($row == 2) {
-                                //$outobj->getActiveSheet()->setCellValue($cols[$i+sizeof($this->files)]."1",$this->filenames[$i] . " Area");
-                            }
                             $area = $vals['Area'][$f];
                             
                             // Calculate color for Area
@@ -232,12 +208,10 @@
                             
                             if ($area > 0) {
                                 // Format area cells
-                                $this->cellColor($cols[$i+sizeof($this->files)].$row,$col,$outobj);
-                                $outobj->getActiveSheet()->getStyle($cols[$i+sizeof($this->files)].$row)->getNumberFormat()->setFormatCode('0.00E+00');
+                                $this->cellColor($cols[$i].$row,$col,$outobj);
+                                $outobj->getActiveSheet()->getStyle($cols[$i].$row)->getNumberFormat()->setFormatCode('0.00E+00');
                             }
-                            $outobj->getActiveSheet()->setCellValue($cols[$i+sizeof($this->files)].$row,$area);
-                        } // end if
-                        
+                            $outobj->getActiveSheet()->setCellValue($cols[$i].$row,$area);
                     } // end if
                     
                 } // end foreach
@@ -265,7 +239,7 @@
             //$r = 255 - $g;
             
             // specify color scale
-            if ($frac < 0.02) {
+            if ($frac < 0.01) {
               $g = 166;
               $r = 111;
             }
@@ -297,34 +271,18 @@
     
 
     
-        public function process_accession($accession,$desc,$unique_peptides,$area=null) {
+        //public function process_accession($accession,$desc,$unique_peptides=null,$area=null) {
+        public function process_accession($accession,$desc,$area=null) {
           if (isset($accession) && $accession != "") {
             
             if (!isset($this->accessions[$accession]))  {
-                $this->accessions[$accession] = array('Description' => $desc, 'Unique_Peptides' => array(),'Area' => array());
+                //$this->accessions[$accession] = array('Description' => $desc, 'Unique_Peptides' => array(),'Area' => array());
+                $this->accessions[$accession] = array('Description' => $desc,'Area' => array());
             }
-            
-            $this->accessions[$accession]['Unique_Peptides'][$this->current_file] = $unique_peptides;
-            
+              
             if (isset($area)) {
                 $this->accessions[$accession]['Area'][$this->current_file] = $area;
             }
-            
-            $accnum = sizeof(array_keys($this->accessions));
-            
-            if ($accnum%100 == 0) {
-                $this->str .= "Found $accnum (th) accession  ".$accession." Min/Max peptides [".$this->minpep."][".$this->maxpep."]\n";
-            }
-              
-            // keep track of min and max values for unique peptides and area
-              
-            if ($unique_peptides < $this->minpep) {
-                $this->minpep = $unique_peptides;
-            }
-            if ($unique_peptides > $this->maxpep) {
-                $this->maxpep = $unique_peptides;
-            }
-              
               
             if ($area < $this->minarea) {
                   $this->minarea = $area;
@@ -344,7 +302,7 @@
     if (isset($argv) && sizeof($argv) > 0) {
         array_shift($argv);
         
-        //print_r($argv);
+        print_r($argv);
         $hmc = new PeptideHeatmapCombiner($argv,"csv");
         $hmc->parseFiles();
         $hmc->combineFiles();
